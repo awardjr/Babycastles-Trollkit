af593cc333153fdaf715a0f201e5ce27640098d4
Rahil627 <Rahil627@gmail.com>
2012-01-11 03:37:51 -0500

    Match the assembly info with the publishing info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BabycastlesRunner")]
+[assembly: AssemblyTitle("Troll Kit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("BabycastlesRunner")]
+[assembly: AssemblyCompany("Babycastles")]
+[assembly: AssemblyProduct("Troll Kit")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
